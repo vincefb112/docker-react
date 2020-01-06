41a1881561b908a5c42611bedf4e531c11a553b7
Vince Bayona <vince.bayona@servicenow.com>
2020-01-06 14:54:29 -0800

    App.js...

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          aaa Edit <code>src/App.js</code> and save to reload. aaa
+          bbb Edit <code>src/App.js</code> and save to reload. bbb
         </p>
         <a
           className="App-link"
